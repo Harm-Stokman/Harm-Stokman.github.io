476c53e0de3c00d67e845ac32661508f01c0d708
Harm-Stokman <harmstok2006@gmail.com>
2026-01-26 11:15:28 +0100

    overzetten

---
+++ script/script.js
@@ -0,0 +1,71 @@
+
+function copying() {
+    var copied = document.getElementById("mail").innerText;
+    navigator.clipboard.writeText(copied)
+}
+
+function dropDown() {
+    document.getElementById("deDrop").classList.toggle("show");
+}
+
+
+let slideIndexAbout = 1;
+toonSlideAbout(slideIndexAbout);
+
+function slideTellenAbout(n) {
+    toonSlideAbout(slideIndexAbout += n);
+}
+
+function huidigeSlideAbout(n) {
+    toonSlideAbout(slideIndexAbout = n);
+}
+
+function toonSlideAbout(n) {
+    let index;
+    let slide = document.getElementsByClassName("about-content");
+
+    if (n > slide.length) {
+        slideIndexAbout = 1
+    }
+
+    if (n < 1)  {
+        slideIndexAbout = slide.length
+    }
+
+    for (index = 0; index < slide.length; index++) {
+        slide[index].style.display = "none"
+    }
+        
+    slide[slideIndexAbout-1].style.display = "flex";
+}
+
+
+let slideIndex = 1;
+toonSlideProject(slideIndex);
+
+function slideTellen(n) {
+    toonSlideProject(slideIndex += n);
+}
+
+function huidigeSlide(n) {
+    toonSlideProject(slideIndex = n);
+}
+
+function toonSlideProject(n) {
+    let i;
+    let slides = document.getElementsByClassName("project-content");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1)  {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+
+    slides[slideIndex-1].style.display = "flex";
+}
