29e56e7d40830e87869f7688c19644aa42550d70
Harm-Stokman <harmstok2006@gmail.com>
2026-01-27 10:41:19 +0100

    laatste verwerking javascript

--- script/script.js
+++ script/script.js
@@ -8,47 +8,37 @@ function dropDown() {
     document.getElementById("deDrop").classList.toggle("show");
 }
 
-
+let slideIndex = 1;
 let slideIndexAbout = 1;
 toonSlideAbout(slideIndexAbout);
 
-function slideTellenAbout(n) {
-    toonSlideAbout(slideIndexAbout += n);
+function slideTellenAbout(a) {
+    toonSlideAbout(slideIndexAbout += a);
 }
 
-function huidigeSlideAbout(n) {
-    toonSlideAbout(slideIndexAbout = n);
-}
-
-function toonSlideAbout(n) {
+function toonSlideAbout(a) {
     let index;
     let slide = document.getElementsByClassName("about-content");
 
-    if (n > slide.length) {
-        slideIndexAbout = 1
+    if (a > slide.length) {
+        slideIndexAbout = 1;
     }
 
-    if (n < 1)  {
-        slideIndexAbout = slide.length
+    if (a < 1)  {
+        slideIndexAbout = slide.length;
     }
 
     for (index = 0; index < slide.length; index++) {
-        slide[index].style.display = "none"
+        slide[index].style.display = "none";
     }
         
     slide[slideIndexAbout-1].style.display = "flex";
 }
 
-
-let slideIndex = 1;
 toonSlideProject(slideIndex);
 
 function slideTellen(n) {
     toonSlideProject(slideIndex += n);
-}
-
-function huidigeSlide(n) {
-    toonSlideProject(slideIndex = n);
 }
 
 function toonSlideProject(n) {
@@ -56,15 +46,15 @@ function toonSlideProject(n) {
     let slides = document.getElementsByClassName("project-content");
 
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
 
     if (n < 1)  {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
+        slides[i].style.display = "none";
     }
 
     slides[slideIndex-1].style.display = "flex";
